ab23cba9b7b5e02a3ac9193e5613bde404e3bf3a
DNB_Dono0530 <donovan1430@gmail.com>
2025-10-03 13:44:20 +0200

    Revert "MateZone V1.0"
    This reverts commit 93e78bef0cd1671a1977bd929a373bf07c5c8ad8.

--- app/MetaZone_code/data/add_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-// filepath: /app/MetaZone_code/data/add_message.php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['msg'])) {
-    file_put_contents('message.data', $_POST['msg'] . PHP_EOL, FILE_APPEND);
-    echo "OK";
-} else {
-    echo "Erreur";
-}
-?>
